2f3da4a648f947d1a21f33df9ef3ec51e7d285d7
wonseok <misaka112@naver.com>
2024-10-14 20:17:53 +0900

    첫째날

---
+++ src/main/java/com/example/amigo_project/AmigoProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.amigo_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmigoProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmigoProjectApplication.class, args);
+    }
+
+}
