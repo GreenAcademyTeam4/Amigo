79ece674cd3708bbcdd2315fb6f2070958e86b08
dbstjlee <dbstjlee@naver.com>
2024-10-17 16:39:30 +0900

    결제 API 완성

---
+++ src/main/java/com/example/amigo_project/dto/payment/ChargeHistoryDTO.java
@@ -0,0 +1,25 @@
+package com.example.amigo_project.dto.payment;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.*;
+
+import java.sql.Timestamp;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class ChargeHistoryDTO {
+
+    private int userId; // user_tb의 PK
+    private String name; // 사용자 이름
+    private String orderName; // 구매 상품
+    private int totalAmount; // 결제 금액
+    private Timestamp approvedAt; // 결제 일시
+    private String orderId; // 주문 번호
+    private String method; // 결제 방식
+    private String paymentKey;
+
+}
---
+++ src/main/java/com/example/amigo_project/repository/interfaces/PaymentRepository.java
@@ -0,0 +1,32 @@
+package com.example.amigo_project.repository.interfaces;
+
+import com.example.amigo_project.repository.model.ChargeHistory;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface PaymentRepository {
+
+    // 결제 내역 생성
+    public void createChargeHistory(ChargeHistory chargeHistory);
+
+    // 포인트 충전
+    public void chargePoint(ChargeHistory chargeHistory);
+
+    // 상품 구매
+    // 상품 선택 --> 구매 --> 포인트 사용 --> 구매 완료
+
+
+    // 환불
+
+
+    // 결제 내역 조회(리스트 및 페이징 처리)
+    public List<ChargeHistory> getAllChargeHistory();
+
+    // 환불 내역(리스트 및 페이징 처리)
+    
+    // 중복 결제 확인
+
+    
+}
