5ce13112ed76f34aecfb57497e7374112dd69344
junsoo <junsoo186@gmail.com>
2024-10-21 10:32:03 +0900

    indexpage

--- src/main/java/com/example/amigo_project/controller/MainController.java
+++ src/main/java/com/example/amigo_project/controller/MainController.java
@@ -2,12 +2,20 @@ package com.example.amigo_project.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 public class MainController {
     @GetMapping("/")
-    public String logincontroller() {
+    public String firstPage() {
 
+        return "first";
+    }
+
+    @GetMapping("/login")
+    public String logincontroller() {
         return "views/login/login";
     }
+    
 }
