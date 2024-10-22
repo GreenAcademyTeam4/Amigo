1feb50bfd62f88e0f232382c97be2da1925b575c
ddg999 <slowman918@gmail.com>
2024-10-22 18:26:49 +0900

    마이페이지 기능 85% 구현

--- src/main/java/com/example/amigo_project/repository/interfaces/UserRepository.java
+++ src/main/java/com/example/amigo_project/repository/interfaces/UserRepository.java
@@ -3,6 +3,7 @@ package com.example.amigo_project.repository.interfaces;
 import com.example.amigo_project.dto.UserDTO;
 import com.example.amigo_project.repository.model.User;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 @Mapper
 public interface UserRepository {
@@ -15,7 +16,10 @@ public interface UserRepository {
     public  boolean checkUserNickname(String nickname);
     // 로그인
     public User loginByUserIdandPassword(UserDTO.loginDTO dto);
-
+    // 비밀번호 변경 로직 -> 기존 비밀번호 1회 입력후 변경 진행을 위한 비밀번호 확인
+    public String findPasswordByUserId(Integer userId);
+    // 비밀번호 변경하기
+    public void updatePasswordByUserId(@Param("userId") Integer userId, @Param("password")String password);
 
 
 }
