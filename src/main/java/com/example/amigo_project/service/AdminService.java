6808b653cf5f39379d985e63f0262047fd6d461a
kimmingyeong225 <kred730036@gmail.com>
2024-10-22 17:57:36 +0900

    게시글, 댓글 삭제, 공지 조회

--- src/main/java/com/example/amigo_project/service/AdminService.java
+++ src/main/java/com/example/amigo_project/service/AdminService.java
@@ -1,6 +1,9 @@
 package com.example.amigo_project.service;
 
+import com.example.amigo_project.dto.AdminDTO;
+import com.example.amigo_project.dto.CommentDTO;
 import com.example.amigo_project.repository.interfaces.AdminRepository;
+import com.example.amigo_project.repository.interfaces.NoticeRepository;
 import com.example.amigo_project.repository.model.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -14,6 +17,7 @@ public class AdminService {
 
 
     private final AdminRepository adminRepository;
+    private final NoticeRepository noticeRepository;
 
     
     public List<User> getUserList(){
@@ -31,6 +35,16 @@ public class AdminService {
 
         return user;
     }
-    
+
+   public AdminDTO findBoardCount(int id) {
+        AdminDTO adminDTO = adminRepository.findBoardCount(id);
+        return adminDTO;
+   }
+
+   public CommentDTO findCommentCount(int id){
+        CommentDTO commentDTO = adminRepository.findCommentCount(id);
+        return commentDTO;
+   }
+
 
 }
