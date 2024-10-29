203471bf52511495fa859b89981676d564997c59
kimmingyeong225 <kred730036@gmail.com>
2024-10-29 13:52:44 +0900

    chart.js 하는 중

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -7,8 +7,9 @@ values('test',1234,'가을','정훈','010-1234-5678','male','27');
 insert into user_tb (user_id,password,name,nickname,phone_number,gender,birth)
 values('test1',1234,'겨울','짱구','010-1234-5678','male','27');
 
-insert into user_tb (user_id,password,name,nickname,phone_number,gender,birth,elementary_school, middle_school, high_school)
-values('diqdiq',1234,'봄','테스트얍','010-1234-5678','male','27', '가초등학교', '나중학교', '다대학교');
+insert into user_tb (user_id,password,name,nickname,phone_number,gender,birth)
+values('test2',1234,'봄','도라에몽','010-1234-5678','female','17');
+
 
 -- 임시 데이터 학교
 INSERT INTO school_tb (id, school, name, region) VALUES
