47c2cb0929a34aaa287ae5d5c0d522be76558b51
wjdgns5 <rhfovkeh5@naver.com>
2024-11-01 20:59:21 +0900

    update- 게시판

--- src/main/java/com/example/amigo_project/dto/CommentDTO.java
+++ src/main/java/com/example/amigo_project/dto/CommentDTO.java
@@ -14,6 +14,7 @@ public class CommentDTO {
 
     private int id;
     private int boardId;
+    private int userId;
     private String nickname;
     private String content;
     private Timestamp createdAt;
@@ -21,8 +22,12 @@ public class CommentDTO {
 
     private Integer CommentCount;
 
+    private boolean isCommentAuthor; // 댓글 작성자 여부 확인을 위한 필드 추가
+
     public void getFormattedCreatedAt() {
         SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         this.createdAtFormat = formatter.format(createdAt);
     }
+
+
 }
